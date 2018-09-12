4cf453b6301b0a0ce26a76ce14993e03444e193b
Moritz Gunz <moritz.gunz@gmail.com>
2018-09-12 18:19:37 +0200

    Rework implementation and add support for other CDN types

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,64 +1,109 @@
+// I heavily used currying here to keep things DRY.
+
 const crypto = require('crypto');
-const Promise = require('bluebird');
 const stringify = require('json-stringify-safe');
 const StoryblokClient = require('storyblok-js-client');
-const chalk = require('chalk');
 
-exports.sourceNodes = ({ boundActionCreators }, options) => {
+/**
+ * Create a function that creates a function that fetches paginated storyblok data from the CDN.
+ *
+ * @param {*} client the storyblok client
+ * @param {*} version the storyblok version to fetch (`draft`, for example)
+ * @param {*} setPluginStatus callback function from gatsby to query the plugin status
+ */
+const createFetcherBase = (client, version, setPluginStatus) => type => { // curry all the things
+  return () => new Promise((res, rej) => {
+    let count = 0;
+    const results = [];
 
-  const { createNode, setPluginStatus } = boundActionCreators;
-
-  const client = new StoryblokClient(options);
-
-  const createAndProcessNodes = (resolve, reject) => {
-    let allStories = [];
-    let lastPage = 1;
-    let getStories = page => {
-      client.get('cdn/stories', {
-        version: options.version,
+    const getItems = (page = 1) => {
+      return client.get(type, {
+        version,
         per_page: 10,
-        page: page
-      }).then(res => {
-        setPluginStatus({ lastFetched: Date.now() });
+        page,
+      })
+        .then(res => {
+          setPluginStatus({ lastFetched: Date.now() });
 
-        let stories = res.data.stories;
-        stories.forEach(story => {
-          allStories.push(story);
+          const keys = Object.keys(res.data);
 
-          story.content = stringify(story.content);
+          if (keys.length > 0) {
+            throw new Error("Got multiple keys in res.data from the API.");
+          }
 
-          let node = Object.assign({}, story, {
-            id: `storyblok-${story.id}`,
-            parent: null,
-            children: [],
-            internal: {
-              mediaType: `application/json`,
-              type: 'StoryblokEntry',
-              contentDigest: crypto.createHash(`md5`).update(stringify(story)).digest(`hex`)
-            }
-          });
+          // If we fetch /stories, we get 'stories' here, if we fetch data sources, we
+          // get `datasource_entries`, etc.. Point is, res.data contains just a single
+          // key and we want to know it.
+          const keyOfItemFetched = keys[0];
 
-          createNode(node);
+          results.push(...res.data[keyOfItemFetched]);
+          count += res.data[keyOfItemFetched].length;
 
-          if (allStories.length == res.total) {
-            resolve({ count: allStories.length });
+          // If we have items left to fetch and its not the last page (in the original sources
+          // both checks were present even though they should be technically equivalent) fetch
+          // the next page.
+          if (count !== res.total && page <= Math.ceil(res.total / res.perPage)) {
+            return getItems(page + 1);
           }
         });
-
-        let total = res.total;
-        lastPage = Math.ceil(res.total / res.perPage);
-
-        if (page <= lastPage) {
-          page++;
-          getStories(page);
-        }
-      }).catch(error => {
-        reject(error);
-      });
     };
 
-    getStories(1);
-  };
+    getItems()
+      .then(() => res(results))
+      .catch(rej);
+  });
+};
 
-  return new Promise(createAndProcessNodes);
+/**
+ * Creates a function that processes items received from storybloks API to be
+ * for gatsby.
+ *
+ * @param {*} createNode the gatsby callback to create a GraphQL node
+ * @param {*} name the name of the node
+ */
+const createItemsProcessor = (createNode, name) => items => {
+  for (const item of items) {
+    if (item.content) { // Special case Story
+      item.content = stringify(item.content);
+    }
+
+    const node = Object.assign({}, item, {
+      id: `storyblok-${item.id}`,
+      parent: null,
+      children: [],
+      internal: {
+        mediaType: `application/json`,
+        type: name,
+        contentDigest: crypto.createHash(`md5`).update(stringify(item)).digest(`hex`)
+      }
+    });
+
+    createNode(node);
+  }
+};
+
+exports.sourceNodes = ({ boundActionCreators }, options) => {
+  const { createNode, setPluginStatus } = boundActionCreators;
+  const client = new StoryblokClient(options);
+
+  // First create the functions that abstract the API pagination
+  const createPaginatedFetcher = createFetcherBase(
+    client,
+    options.version,
+    setPluginStatus,
+  );
+
+  const fetchDataSources = createPaginatedFetcher('cdn/datasource_entries');
+  const fetchStories = createPaginatedFetcher('cdn/stories');
+  const fetchTags = createPaginatedFetcher('cdn/tags');
+
+  // Then insert everything into GraphQL
+  const fetchingDataSources = fetchDataSources()
+    .then(createItemsProcessor(createNode, 'StoryblokDataSource'));
+  const fetchingStories = fetchStories()
+    .then(createItemsProcessor(createNode, 'StoryblokEntry'));
+  const fetchingTags = fetchTags()
+    .then(createItemsProcessor(createNode, 'StoryblokTag'));
+
+  return Promise.all([fetchingDataSources, fetchingStories, fetchingTags]);
 };
