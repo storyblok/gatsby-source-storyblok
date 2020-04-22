d356baff7c22b991aecf6f2e3f57a01dd8e22dbb
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-04-22 14:27:18 -0300

    refactor getStoryParams function to use resolveLinks options when is passed

--- src/getStoryParams.js
+++ src/getStoryParams.js
@@ -1,8 +1,16 @@
-module.exports = function(language, options) {
+/**
+ * @method getStoryParams
+ * @param  {String}        language 'en', 'de'
+ * @param  {Object}        options?  api options
+ * @param  {Array<String>} options.resolveRelations? resolve_relations field
+ * @param  {String}        options.resolveLinks? can be story or url
+ * @param  {String}        options.version? can be draft or released
+ */
+const getStoryParams = function(language = '', options = {}) {
   let params = {}
 
   if (options.resolveLinks) {
-    params.resolve_links = '1'
+    params.resolve_links = options.resolveLinks || '1'
   }
 
   if (options.resolveRelations) {
@@ -19,3 +27,5 @@ module.exports = function(language, options) {
 
   return params
 }
+
+module.exports = getStoryParams
