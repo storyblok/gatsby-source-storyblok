c6f2d186dd625b3f8200ed1a3916a64897c3d44e
schabibi1 <arisagse@gmail.com>
2023-06-02 15:55:43 +0200

    chore: server side test

--- playground/src/components/Feature.jsx
+++ playground/src/components/Feature.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use server"// TRY OUT THIS LINE TO SEE IF IT FAILS
+import React, { useEffect } from "react";
 import { storyblokEditable } from "gatsby-source-storyblok";
 
 const Feature = ({ blok }) => {
+  useEffect(() => {
+    console.log("Feature component mounted"); // THIS SHOULD FAIL OR BE IGNORED
+  });
   return (
     <div className="py-2" {...storyblokEditable(blok)} key={blok._uid} data-test="feature">
       <h2 className="text-lg">{blok.name}</h2>
